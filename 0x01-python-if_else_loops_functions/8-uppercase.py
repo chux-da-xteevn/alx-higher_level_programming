bab614d0b0141405a3b360972eeafcba5b90a8ec
chux-da-xteevn <chukwu.steevn@gmail.com>
2024-01-04 07:44:54 -0800

    Task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def to_upper(character):
+    if ord(character) >= 97 and ord(character) <= 122:
+        return (ord(character) - 32)
+    else:
+        return ord(character)
+
+
+
+def uppercase(str):
+    new = ""
+    for character in str:
+        new += "%c" % to_upper(character)
+    print("{:s}".format(new))
