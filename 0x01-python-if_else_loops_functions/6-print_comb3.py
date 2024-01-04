e50e46a52137f63daaa7391f4941af19493d5292
chux-da-xteevn <chukwu.steevn@gmail.com>
2024-01-04 06:41:10 -0800

    Task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 num = 0
-while num <= 90:
+while num <= 89:
     if num % 10 == 0:
         num += 1 + num // 10
-        print("{:02d}".format(num), end='\n' if num == 90 else ", ")
-        num += 1
+    print("{:02d}".format(num), end='\n' if num == 89 else ", ")
+    num += 1
