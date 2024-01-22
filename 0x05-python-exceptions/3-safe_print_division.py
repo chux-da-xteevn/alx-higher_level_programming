13a7fea73ab442565f1eabbfd50f775da8f2059d
chux-da-xteevn <chukwu.steevn@gmail.com>
2024-01-22 15:20:35 -0800

    Task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,20 @@
-#!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        div = a / b
-    except ZeroDivisionError
-         div = None
+        divide = a / b
+    except ZeroDivisionError:
+        divide = None
     finally:
-        print("Inside result: {}".format(div))
-    return div
+        print("Inside result: {}".format(divide))
+        return divide
+
+# Test cases
+if __name__ == "__main__":
+    a = 12
+    b = 2
+    divide = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, divide))
+
+    a = 12
+    b = 0
+    divide = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, divide))
