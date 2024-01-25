ba56446bbbbceaff948245f51af41aafeea3fb6e
chux-da-xteevn <chukwu.steevn@gmail.com>
2024-01-24 18:51:46 -0800

    Task 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
+""" class to define square"""
+
 
 class Square:
+    """
+    Representation of a square.
+    """
     def __init__(self, size=0):
-        self.__size = size
+        """
+        the init function that gets invoked when a new instance is created
+        :param size:
+        """
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-
-    def area(self):
-        return self.__size ** 2
-
+        self.__size = size
