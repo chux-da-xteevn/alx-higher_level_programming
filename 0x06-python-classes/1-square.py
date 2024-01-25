612b80e823a83f103db173a7d519f8b041c64adf
chux-da-xteevn <chukwu.steevn@gmail.com>
2024-01-24 17:03:35 -0800

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+
+
+"""Define a class Square here."""
+
+
+
+
 class Square:
-    
+    """Represent a square."""
+
 
     def __init__(self, size):
         """Initializing a new Square.
