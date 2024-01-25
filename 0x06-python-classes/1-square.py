014a5c4a7ad29ee2cf31c2b612953b386d5bf02e
chux-da-xteevn <chukwu.steevn@gmail.com>
2024-01-24 16:55:42 -0800

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    
+
+    def __init__(self, size):
+        """Initializing a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
