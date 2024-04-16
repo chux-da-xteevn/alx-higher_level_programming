99cc41b0b3a164f2c131775d5c0a45ba70b0a5d5
chux-da-xteevn <chukwu.steevn@gmail.com>
2024-04-16 04:13:18 +0100

    Task 102

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+All states via SQLAlchemy
+"""
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    all_city = session.query(City).all()
+
+    for k in all_city:
+        print(f"{k.id}: {k.name} -> {k.state.name}")
